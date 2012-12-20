4abfb86b66b53cdd5e1897a25178bfd7e4283c8e
charleschu <charles_chu@foxmail.com>
2012-12-20 10:23:17 +0800

    use config to load custom module

---
+++ config/config.js
@@ -0,0 +1,34 @@
+YUI.GlobalConfig = {
+  groups: {
+    views: {
+      base: '/javascript/views/',
+      modules:{
+        campaignView: {
+          path: '/campaign-view.js'
+        },
+        designView: {
+          path: '/design-view.js'
+        },
+        pageView: {
+          path: '/page-view.js'
+        }
+      }
+    },
+    models: {
+      base: '/javascript/models/',
+      modules:{
+        emailModel: {
+          path: '/email-model.js'
+        }
+      }
+    },
+    app: {
+      base: '/javascript/',
+      modules: {
+        emailApp: {
+          path: '/app.js'
+        }
+      }
+    }
+  }
+}
